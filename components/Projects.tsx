7d370aab12ef856101e3bf385343a7eb7cba4bdb
Nooreen Fatima <nooreenfatima@Mac.attlocal.net>
2025-12-28 22:04:54 -0600

    refine

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,44 @@ import FluidGlass from "./FluidGlass";
 
 export default function Projects() {
     const [showMore, setShowMore] = useState(false);
+    const projectsData = [
+        {
+            title: "Course Management System (Java)",
+            github: "https://github.com/nooreenf16/Course-Management",
+            imgPath: "assets/temp.png",
+            desc: "Developed a full-stack course management system (repo: Course-Management), using Java for backend logic and HTML for frontend, enabling CRUD operations for students, courses and enrollments. • Designed architecture for scalability and maintainability: clear separation of concerns, data models and user interface.",
+            tools: "Java, Springboot, Kafka, ReactJS"
+        },
+        {
+            title: "Multiple Partial Object Detection in a Single Image using Segmentation and Elliptical Symmetry - Research",
+            github: "https://github.com/nooreenf16/Multiple-Partial-Object-Detection",
+            imgPath: "assets/temp.png",
+            desc: "A software which uses image processing and geometric operations to identify the location & labeling of objects in image • Implemented using segmentation of all objects, image reconstruction and performing a detection algorithm.",
+            tools: "Python, Numpy, openCV, Math."
+        },
+        {
+            title: "Event Management Website ( Full Stack )",
+            github: "https://github.com/nooreenf16/EventManagement",
+            imgPath: "assets/temp.png",
+            desc: "• A website used to manage all events on user and admin end with dynamic user interface and multiple endpoints. • Designed responsive frontend using Figma, HTML, CSS, JavaScript, Bootstrap. • Integrated Flask backend, MySQL database, and REST APIs for data management and authentication services.",
+            tools: ""
+        },
+        {
+            title: "Vehicle Classification and Counting",
+            github: "https://github.com/nooreenf16/Vehicle-Classification-and-Counting",
+            imgPath: "assets/temp.png",
+            desc: "• Authored a Python solution (repo: Vehicle-Classification-and-Counting) for classNameifying vehicles and counting traffic flow from video/image data—useful in smart-city and traffic-monitoring contexts. • Processed real-world data, extracted features, and developed evaluation metrics to validate classNameification and counting accuracy.",
+            tools: "Python, Numpy, openCV, Math, YOLO algorithm."
+        },
+        {
+            title: "NLP Project",
+            github: "https://github.com/nooreenf16/NLP",
+            imgPath: "assets/temp.png",
+            desc: "• Built a collection of machine-learning and data-science projects (repo: ML-and-DS) using Jupyter notebooks: data cleaning, feature engineering, model training and evaluation across different datasets. • Showcased versatility in regression, classNameification and clustering workflows.",
+            tools: "Python, Numpy, Pandas, Scikit, Matplotlib, Machine Learning Algorithms"
+        }
+        //ds port
+    ]
     return (
         //<section className="m-10 bg-[radial-gradient(circle_1000px_at_center,_#500140_0%,#0A0A0A_80%)]" id="projects"> */}
         <section className="mt-10 ml-10 bg-gradient-to-br from-[#0a0a0a] via-[#0a0a0a] to-[#500140]" id="projects">
@@ -13,89 +51,15 @@ export default function Projects() {
             </h1>
 
             <div className="mt-10 grid grid-cols-[repeat(auto-fit,_minmax(450px,_1fr))] gap-6">
-                <div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
-                    <h3 className="font-bold mb-2 text-xl">Course Management System (Java)</h3>
-                    {/* <img className="rounded my-5" src={"assets/temp.webp"} alt="Demo img"></img> */}
+                {projectsData.slice(0, 3).map((p) =>
+                    <div key={p.github} className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
+                        <h3 className="font-bold mb-2 text-xl">
+                            {p.title}
+                            <a className="text-pink-400 text-xs
+                         rounded-xl border-[1px] border-white/20 p-1 m-2"
+                                href={p.github}>GitHub Link</a>
+                        </h3>
 
-                    <div style={{ height: "200px" }}>
-                        <FluidGlass
-                            mode="lens" // or "bar", "cube"
-                            lensProps={{
-                                scale: 0.25,
-                                ior: 1.15,
-                                thickness: 5,
-                                chromaticAberration: 0.1,
-                                anisotropy: 0.01
-                            }}
-                            imgSrc="assets/temp.webp"
-                        // cubeProps={} // add specific props if using cube mode
-                        />
-                    </div>
-
-                    <p>Developed a full-stack course management system (repo: Course-Management), using Java for backend logic and HTML for frontend, enabling CRUD operations for students, courses and enrollments.
-                        • Designed architecture for scalability and maintainability: clear separation of concerns, data models and user interface.</p>
-                    <hr className="my-2 border-gray-200/30 border-t-[0.5px]" />
-                    <p className="text-transparent bg-clip-text bg-gradient-to-r 
-                from-purple-500 via-pink-500 to-purple-500 text-sm text-center">
-                        Java, Springboot, Kafka, ReactJS</p>
-                </div>
-
-                <div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl p-8 shadow-lg rounded-lg shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
-                    <h3 className="font-bold mb-2 text-xl">Multiple Partial Object Detection in a Single Image using Segmentation and Elliptical Symmetry - Research GitHub
-                    </h3>
-                    {/* <img className="rounded my-5" src={"assets/temp.webp"} alt="Demo img"></img> */}
-                    <div style={{ height: "200px" }}>
-                        <FluidGlass
-                            mode="lens" // or "bar", "cube"
-                            lensProps={{
-                                scale: 0.25,
-                                ior: 1.15,
-                                thickness: 5,
-                                chromaticAberration: 0.1,
-                                anisotropy: 0.01
-                            }}
-                            imgSrc="assets/temp.webp"
-                        // cubeProps={} // add specific props if using cube mode
-                        />
-                    </div>
-
-                    <p>A software which uses image processing and geometric operations to identify the location & labeling of objects in image
-                        • Implemented using segmentation of all objects, image reconstruction and performing a detection algorithm.</p>
-                    <hr className="my-2 border-gray-200/30 border-t-[0.5px]" />
-                    <p className="text-transparent bg-clip-text bg-gradient-to-r 
-                from-purple-500 via-pink-500 to-purple-500 text-sm text-center">
-                        Python, Numpy, openCV, Math.</p>
-                </div>
-
-                <div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl p-8 shadow-lg rounded-lg shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
-                    <h3 className="font-bold mb-2 text-xl">Event Management Website ( Full Stack )</h3>
-                    {/* <img className="rounded my-5" src={"assets/temp.webp"} alt="Demo img"></img> */}
-                    <div style={{ height: "200px" }}>
-                        <FluidGlass
-                            mode="lens" // or "bar", "cube"
-                            lensProps={{
-                                scale: 0.25,
-                                ior: 1.15,
-                                thickness: 5,
-                                chromaticAberration: 0.1,
-                                anisotropy: 0.01
-                            }}
-                            imgSrc="assets/temp.webp"
-                        // cubeProps={} // add specific props if using cube mode
-                        />
-                    </div>
-                    <p>• A website used to manage all events on user and admin end with dynamic user interface and multiple endpoints.
-                        • Designed responsive frontend using Figma, HTML, CSS, JavaScript, Bootstrap.
-                        • Integrated Flask backend, MySQL database, and REST APIs for data management and authentication services.</p>
-                    <hr className="my-2 border-gray-200/30 border-t-[0.5px]" />
-
-                    <p className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-pink-500 to-purple-500 text-sm text-center">Python, Flask, Bootstrap, MySQL, UML, REST APIs, MongoDB</p>
-                </div>
-
-                {showMore &&
-                    <><div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl p-8 shadow-lg rounded-lg shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
-                        <h3 className="font-bold mb-2 text-xl">Vehicle Classification and Counting</h3>
-                        {/* <img className="rounded my-5" src={"assets/temp.webp"} alt="Demo img"></img> */}
                         <div style={{ height: "200px" }}>
                             <FluidGlass
                                 mode="lens" // or "bar", "cube"
@@ -106,49 +70,24 @@ export default function Projects() {
                                     chromaticAberration: 0.1,
                                     anisotropy: 0.01
                                 }}
-                                imgSrc="assets/temp.webp"
-                            // cubeProps={} // add specific props if using cube mode
+                                imgSrc={p.imgPath}
                             />
                         </div>
 
-
-                        <p>• Authored a Python solution (repo: Vehicle-Classification-and-Counting) for classNameifying vehicles and counting traffic flow from video/image data—useful in smart-city and traffic-monitoring contexts.
-                            • Processed real-world data, extracted features, and developed evaluation metrics to validate classNameification and counting accuracy.</p>
+                        <p>{p.desc}</p>
                         <hr className="my-2 border-gray-200/30 border-t-[0.5px]" />
                         <p className="text-transparent bg-clip-text bg-gradient-to-r 
                 from-purple-500 via-pink-500 to-purple-500 text-sm text-center">
-                            Python, Numpy, openCV, Math, YOLO algorithm.</p>
+                            {p.tools}</p>
                     </div>
+                )}
 
-                        <div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl p-8 shadow-lg rounded-lg shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
-                            <h3 className="font-bold mb-2 text-xl">NLP Project</h3>
-                            {/* <img className="rounded my-5" src={"assets/temp.webp"} alt="Demo img"></img> */}
-                            <div style={{ height: "200px" }}>
-                                <FluidGlass
-                                    mode="lens" // or "bar", "cube"
-                                    lensProps={{
-                                        scale: 0.25,
-                                        ior: 1.15,
-                                        thickness: 5,
-                                        chromaticAberration: 0.1,
-                                        anisotropy: 0.01
-                                    }}
-                                    imgSrc="assets/temp.webp"
-                                // cubeProps={} // add specific props if using cube mode
-                                />
-                            </div>
-
-                            <p>• Built a collection of machine-learning and data-science projects (repo: ML-and-DS) using Jupyter notebooks: data cleaning, feature engineering, model training and evaluation across different datasets.
-                                • Showcased versatility in regression, classNameification and clustering workflows.</p>
-                            <hr className="my-2 border-gray-200/30 border-t-[0.5px]" />
-                            <p className="text-transparent bg-clip-text bg-gradient-to-r 
-                from-purple-500 via-pink-500 to-purple-500 text-sm text-center">
-                                Python, Numpy, Pandas, Scikit, Matplotlib, Machine Learning Algorithms</p>
-                        </div>
+                {showMore &&
+                    <>
 
                         <div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl p-8 shadow-lg rounded-lg shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
                             <h3 className="font-bold mb-2 text-xl">ML & Data-Science Portfolio</h3>
-                            {/* <img className="rounded my-5" src={"assets/temp.webp"} alt="Demo img"></img> */}
+                            {/* <img className="rounded my-5" src={"assets/temp.png"} alt="Demo img"></img> */}
                             <div style={{ height: "200px" }}>
                                 <FluidGlass
                                     mode="lens" // or "bar", "cube"
@@ -159,7 +98,7 @@ export default function Projects() {
                                         chromaticAberration: 0.1,
                                         anisotropy: 0.01
                                     }}
-                                    imgSrc="assets/temp.webp"
+                                    imgSrc="assets/temp.png"
                                 // cubeProps={} // add specific props if using cube mode
                                 />
                             </div>
@@ -174,7 +113,7 @@ export default function Projects() {
 
                         <div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl p-8 shadow-lgrounded-lg shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
                             <h3 className="font-bold mb-2 text-xl">LLM Workshop / Cursor Integration / Firebase Studio</h3>
-                            {/* <img className="rounded my-5" src={"assets/temp.webp"} alt="Demo img"></img> */}
+                            {/* <img className="rounded my-5" src={"assets/temp.png"} alt="Demo img"></img> */}
                             <div style={{ height: "200px" }}>
                                 <FluidGlass
                                     mode="lens" // or "bar", "cube"
@@ -185,7 +124,7 @@ export default function Projects() {
                                         chromaticAberration: 0.1,
                                         anisotropy: 0.01
                                     }}
-                                    imgSrc="assets/temp.webp"
+                                    imgSrc="assets/temp.png"
                                 // cubeProps={} // add specific props if using cube mode
                                 />
                             </div>
