8c6e5cbf9990863ffbbf29c3956ff0dc04ca42f3
Nooreen Fatima <nooreenfatima@Mac.attlocal.net>
2025-12-29 10:53:54 -0600

    imgs

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
         {
             title: "Course Management System (Java)",
             github: "https://github.com/nooreenf16/Course-Management",
-            imgPath: "assets/temp.png",
+            imgPath: "",
             desc: "Developed a full-stack course management system (repo: Course-Management), using Java for backend logic and HTML for frontend, enabling CRUD operations for students, courses and enrollments. • Designed architecture for scalability and maintainability: clear separation of concerns, data models and user interface.",
             tools: "Java, Springboot, Kafka, ReactJS"
         },
@@ -42,6 +42,7 @@ export default function Projects() {
             tools: "Python, Numpy, Pandas, Scikit, Matplotlib, Machine Learning Algorithms"
         }
         //ds port
+        //https://badriadhikari.github.io/llm-workshop-2025/
     ]
     return (
         //<section className="m-10 bg-[radial-gradient(circle_1000px_at_center,_#500140_0%,#0A0A0A_80%)]" id="projects"> */}
@@ -58,9 +59,13 @@ export default function Projects() {
                             <a className="text-pink-400 text-xs
                          rounded-xl border-[1px] border-white/20 p-1 m-2"
                                 href={p.github}>GitHub Link</a>
+
+                            {p.imgPath == "assets/beeproject.png" && <a className="text-pink-400 text-xs
+                         rounded-xl border-[1px] border-white/20 p-1 m-2"
+                                href={"https://drive.google.com/file/d/1fcBucC_SYXiFgrJcMH1fdo9lJAS7QGcE/view?usp=sharing"}>Preview Link</a>}
                         </h3>
 
-                        <div style={{ height: "200px" }}>
+                        {p.imgPath != "" && <div style={{ height: "200px" }}>
                             <FluidGlass
                                 mode="lens" // or "bar", "cube"
                                 lensProps={{
@@ -72,7 +77,7 @@ export default function Projects() {
                                 }}
                                 imgSrc={p.imgPath}
                             />
-                        </div>
+                        </div>}
 
                         <p>{p.desc}</p>
                         <hr className="my-2 border-gray-200/30 border-t-[0.5px]" />
@@ -88,20 +93,9 @@ export default function Projects() {
                         <div className="mx-5 bg-white/5 backdrop-blur-md border border-white/20 rounded-xl p-8 shadow-lg rounded-lg shadow-lg p-6 text-[#eee] transition-transform duration-300 ease-in-out hover:scale-105">
                             <h3 className="font-bold mb-2 text-xl">ML & Data-Science Portfolio</h3>
                             {/* <img className="rounded my-5" src={"assets/temp.png"} alt="Demo img"></img> */}
-                            <div style={{ height: "200px" }}>
-                                <FluidGlass
-                                    mode="lens" // or "bar", "cube"
-                                    lensProps={{
-                                        scale: 0.25,
-                                        ior: 1.15,
-                                        thickness: 5,
-                                        chromaticAberration: 0.1,
-                                        anisotropy: 0.01
-                                    }}
-                                    imgSrc="assets/temp.png"
-                                // cubeProps={} // add specific props if using cube mode
-                                />
-                            </div>
+                            <a className="text-pink-400 text-xs
+                         rounded-xl border-[1px] border-white/20 p-1 m-2"
+                                href={"https://github.com/nooreenf16/ML-and-DS"}>GitHub Link</a>
 
                             <p>• Conducted exploratory work in natural language processing (repo: NLP) including topic modeling, sentiment analysis and text-classNameification pipelines using Python.
                                 • Demonstrated ability to preprocess text, apply NLP frameworks, and evaluate model results.</p>
@@ -124,7 +118,7 @@ export default function Projects() {
                                         chromaticAberration: 0.1,
                                         anisotropy: 0.01
                                     }}
-                                    imgSrc="assets/temp.png"
+                                    imgSrc="assets/llmworkshop.png"
                                 // cubeProps={} // add specific props if using cube mode
                                 />
                             </div>
